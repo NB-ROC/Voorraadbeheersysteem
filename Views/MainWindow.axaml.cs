90c1960a199b7ed90ec2e40accb1388b8772a257
A-A-Tel <anouaranthonytel@gmail.com>
2026-02-23 19:42:32 +0100

    init

---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace StorageBorrowManagement.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
