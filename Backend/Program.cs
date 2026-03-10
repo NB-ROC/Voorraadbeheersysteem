66bb0fe5e5764afb63a35db5264526205e25c7a6
A-A-Tel <anouaranthonytel@gmail.com>
2026-03-10 22:59:53 +0100

    added user services and partial methods

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,16 +1,29 @@
 using Backend.Database;
 using Backend.Services;
+using Microsoft.EntityFrameworkCore;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+var env = builder.Environment.EnvironmentName;
 
 builder.Services.AddGrpc();
 
-builder.Services.AddDbContext<AppDbContext>();
+if (env == "Test")
+{
+    builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseInMemoryDatabase("testing")); // TODO: Make this actually work lmao
+}
+else
+{
+    builder.Services.AddDbContext<AppDbContext>();
+}
 
-builder.Services.AddScoped<GreeterService>();
+builder.Services.AddDbContext<AppDbContext>();
 
 WebApplication app = builder.Build();
 
+app.Services.CreateScope().ServiceProvider.GetRequiredService<AppDbContext>().Database.EnsureCreated();
+
 app.MapGrpcService<GreeterService>();
+app.MapGrpcService<UserService>();
 
 app.Run();
