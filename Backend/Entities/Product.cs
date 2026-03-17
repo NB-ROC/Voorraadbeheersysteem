8c87fc3579e2e234350a4af9273bb89f1a262ff7
A-A-Tel <anouaranthonytel@gmail.com>
2026-03-17 19:29:58 +0100

    added maxlength properties to strings

--- Backend/Entities/Product.cs
+++ Backend/Entities/Product.cs
@@ -1,14 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Backend.Entities;
 
 public class Product
 {
     public int Id { get; set; }
 
-    public string Name { get; set; } = null!;
+    [MaxLength(64)] public string Name { get; set; } = null!;
 
-    public string Category { get; set; } = null!;
+    [MaxLength(45)] public string Category { get; set; } = null!;
 
-    public string Description { get; set; } = null!;
+    [MaxLength(128)] public string Description { get; set; } = null!;
 
     public byte Amount { get; set; }
 
--- Backend/Entities/User.cs
+++ Backend/Entities/User.cs
@@ -1,12 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Backend.Entities;
 
 public class User
 {
     public byte[] Id { get; set; } = new byte[7];
 
-    public string Email { get; set; } = null!;
+    [MaxLength(255)] public string Email { get; set; } = null!;
 
-    public string Name { get; set; } = null!;
+    [MaxLength(32)] public string Name { get; set; } = null!;
 
     public ushort Number { get; set; }
 
