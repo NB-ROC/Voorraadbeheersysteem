9d1b42a372aeb4aadd63e28fd1c1efec2e71d79d
ahawaaye <ahawa004@gmail.com>
2026-04-14 13:13:29 +0200

    init Login

---
+++ FrontendAdmin/ViewModels/Login/LoginPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace FrontendAdmin.ViewModels.Login;
+
+public class LoginPageViewModel : PageViewModelBase
+{
+    public LoginPageViewModel(ServiceProvider services) : base(services)
+    {
+    }
+    
+    
+}
