919ae52caa713f78213dd8910e451ed97229e355
ahawaaye <ahawa004@gmail.com>
2026-04-30 18:27:20 +0200

    complete user story 1 + fix constructor errors and new design

--- FrontendAdmin/ViewModels/Login/LoginPageViewModel.cs
+++ FrontendAdmin/ViewModels/Login/LoginPageViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Reactive;
+using FrontendAdmin.Services;
 using Microsoft.Extensions.DependencyInjection;
 using ReactiveUI;
 
@@ -7,23 +8,51 @@ namespace FrontendAdmin.ViewModels.Login;
 
 public class LoginPageViewModel : PageViewModelBase
 {
+    private string _username = string.Empty;
+    private string _password = string.Empty;
+    private string _email = string.Empty;
+    private string _errorMessage = string.Empty;
+
     public LoginPageViewModel(ServiceProvider services) : base(services)
     {
-        LoginCommand = ReactiveCommand.Create(() =>
-        {
-            Console.WriteLine("command executed");
-        });
+        var canLogin = this.WhenAnyValue(
+            x => x.Username,
+            x => x.Password,
+            (u, p) => !string.IsNullOrWhiteSpace(u) && !string.IsNullOrWhiteSpace(p)
+            );
+        
+        LoginCommand = ReactiveCommand.Create(Login, canLogin);
+    }
+
+    public string Username
+    {
+        get => _username;
+        set => this.RaiseAndSetIfChanged(ref _username, value);
+    }
+
+    public string Password
+    {
+        get => _password;
+        set => this.RaiseAndSetIfChanged(ref _password, value);
+    }
+
+    public string Email
+    {
+        get => _email;
+        set => this.RaiseAndSetIfChanged(ref _email, value);
+    }
+
+    public string ErrorMessage
+    {
+        get => _errorMessage;
+        set => this.RaiseAndSetIfChanged(ref _errorMessage, value);
     }
     
-    public string Username { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
     public ReactiveCommand<Unit, Unit> LoginCommand { get; }
-        
-    public string ErrorMessage { get; set; } = string.Empty;
 
-    public void Login()
+    private void Login()
     {
-            
+        Console.WriteLine($"Login clicked: {Username}/ {Password}");
     }
-
+    
 }
