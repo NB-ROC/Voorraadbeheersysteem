c414626f5a903bc2c69432ccab2631e0773a1f7e
a-a-tel <anouaranthonytel@gmail.com>
2026-09-11 20:48:44 +0200

    fixed connection bugs

--- Backend/Database/AppDbContext.cs
+++ Backend/Database/AppDbContext.cs
@@ -8,7 +8,7 @@ namespace Backend.Database;
 
 public class AppDbContext : DbContext
 {
-    protected virtual string CurrentDatetimeSyntax => "UTC_TIMESTAMP(6)";
+    protected virtual string CurrentDatetimeSyntax => "CURRENT_TIMESTAMP(6)";
 
     public DbSet<User> Users { get; set; }
     public DbSet<Role> Roles { get; set; }
@@ -121,7 +121,7 @@ public class AppDbContext : DbContext
             builder.Entity<User>()
                 .Property(x => x.UpdatedAt)
         );
-        
+
         builder.Entity<Role>()
             .Property(x => x.CreatedAt)
             .ValueGeneratedOnAdd()
@@ -210,7 +210,7 @@ public class AppDbContext : DbContext
             Port = GetEnvIntUnsigned("DB_PORT"),
             Database = GetEnv("DB_NAME"),
             UserID = GetEnv("DB_USERNAME"),
-            Password = GetEnv("DB_PASSWORD")
+            Password = GetEnv("DB_PASSWORD"),
         };
 
         options.UseMySql(builder.ConnectionString, ServerVersion.AutoDetect(builder.ConnectionString));
